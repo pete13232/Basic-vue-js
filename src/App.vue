008bc894cdf4c9f7e513cbc3265f2ae6ca6e3430
Pete <peetawit.vong@mail.kmutt.ac.th>
2022-06-21 01:05:33 +0700

    Finish First Phase

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <section>
     <img :src="picture" alt="pic" :width="size" :height="size" ref="imageEl" />
-    <form @submit.prevent="submitForm">
-      <label>ป้อนชื่อเล่น:</label>
-      <input type="text" ref="nickNameEl" />
-      <button type="submit">บันทึก</button>
-    </form>
-    <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
-    <h1>ชื่อเล่น : {{ nickName }}</h1>
+    <h1>ชื่อ-นามสกุล : {{ getFullName }}</h1>
     <h1>อายุ : {{ age }}</h1>
-    <p>ที่อยู่ : <span v-html="address"></span></p>
-    <p><a :href="social" target="_blank">Social</a></p>
-    <p>งานอดิเรก</p>
-    <ul>
-      <li>{{ hobby[0] }}</li>
-      <li>{{ hobby[1] }}</li>
-      <li>{{ hobby[2] }}</li>
-    </ul>
-    <p>ข้อมูลพื้นฐาน</p>
-    <ul>
-      <li>เพศ: {{ general.gender }}</li>
-      <li>น้ำหนัก: {{ general.weight }} กิลโลกรัม</li>
-      <li>ความสูง: {{ general.height }} cm.</li>
-      <li>โรคประจำตัว: {{ general.status }}</li>
-    </ul>
-    <button @click="showData()">คลิกเพื่อดูข้อมูล</button>
-    <button @click.ctrl="increment(10)">เพิ่ม</button>
-    <button @click.middle="decrement(5)">ลด</button>
+    <h1>เงินเดือน : {{ salary }} บาท</h1>
+    <h1>รายได้ต่อปี : {{ getIncome }} บาท</h1>
+    <h1>ตำแหน่งงาน : {{ getDepartment }}</h1>
+    <button @click="addSalary(5000)">เพิ่มเงินเดือน</button>
+    <button @click="toggleVisible">
+      {{ isVisble ? "ซ่อน" : "แสดง" }}รายละเอียด
+    </button>
+    <article v-show="isVisble">
+      <p>ที่อยู่ : <span v-html="address"></span></p>
+      <p><a :href="social" target="_blank">Social</a></p>
+      <p v-if="hobby.length === 0">ไม่มีงานอดิเรก</p>
+      <div v-else>
+        <p>งานอดิเรก :</p>
+        <ul>
+          <li v-for="(item, index) in hobby" :key="index">
+            {{ index }}-{{ item }}
+          </li>
+        </ul>
+      </div>
+      <p>ข้อมูลพื้นฐาน</p>
+      <ul>
+        <li v-for="(item, key) in general" :key="key">
+          {{ key }} : {{ item }}
+        </li>
+      </ul>
+    </article>
   </section>
 </template>
 
@@ -44,30 +46,44 @@ export default {
         "https://as2.ftcdn.net/v2/jpg/02/29/75/83/1000_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
       size: 50,
       social: "https://www.facebook.com/kongruksiamtutorial",
-      hobby: ["ทำสวน", "เล่นเกม", "ฟังเพลง"],
+      hobby: ["ทำสวน", "เล่นเกม", "ฟังเพลง", "ทำอาหาร", "เลี้ยงแมว"],
       general: {
         gender: "ชาย",
         weight: "70.4",
         height: "170",
         status: false,
       },
+      isVisble: false,
+      salary: 20000,
     };
   },
   methods: {
+    toggleVisible() {
+      this.isVisble = !this.isVisble;
+    },
+    addSalary(value) {
+      this.salary += value;
+    },
+  },
+  computed: {
     getFullName() {
       return `${this.firstName} ${this.lastName}`;
     },
-    showData() {
-      alert(this.firstName);
+    getIncome() {
+      return this.salary * 12;
     },
-    increment(value) {
-      this.age += value;
+    getDepartment() {
+      return this.salary >= 35000 ? "Project manager" : "Programmer";
     },
-    decrement(value) {
-      this.age -= value;
-    },
-    submitForm() {
-      this.nickName = this.$refs.nickNameEl.value;
+  },
+  watch: {
+    salary(value) {
+      if (value > 50000) {
+        alert("เงินเดือนไม่ควรเกิน 50,000 บาท");
+        setTimeout(()=>{
+          this.salary = 50000
+        },100)
+      }
     },
   },
 };
